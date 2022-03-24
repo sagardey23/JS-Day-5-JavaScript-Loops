4fcb0b9374d5bc7c90c6df4c0d08f041b088adb1
sagardey23 <sagarinthesky@gmail.com>
2022-03-24 14:12:26 +0530

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+// for loop
+
+// var num= 5;
+
+// for (let i=0; i<num; i++){
+//     console.log(i)
+// }
+
+// for (i=1; i<=5; i++)  
+// {  
+// document.write(i + "<br/>")  
+// }  
+
+
+//  table of 13
+
+// var num=13;
+
+// for (let i=1; i<=10; i++){
+//     console.log("13"+"X"+i +"=" + num*i)
+// }
+
+
+
+
+//  while loop
+
+// var i=11;
+
+// while(i<=15){
+//     console.log(i);
+//     i++;
+
+// }
+
+
+
